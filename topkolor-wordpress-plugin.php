b5501a540c4bcbe4fb969485a5657b9172cd0ad6
Larik <turolretar@gmail.com>
2020-08-13 14:14:49 +0300

    Updated Plugin

--- topkolor-wordpress-plugin.php
+++ topkolor-wordpress-plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: TOPKOLOR Plugin
  * Author: Larik
- * Version: 0.0.1
+ * Version: 0.0.6
  * GitHub Plugin URI: https://github.com/Larento/topkolor-wordpress-plugin
  * License: GNU General Public License v2 or later
  */
