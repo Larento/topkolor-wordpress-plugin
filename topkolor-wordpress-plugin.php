6d97b5c8133bc12f1d80b0e1eeb10d1b77f31c3c
Larik <turolretar@gmail.com>
2020-08-13 14:43:13 +0300

    Updated Plugin

--- topkolor-wordpress-plugin.php
+++ topkolor-wordpress-plugin.php
@@ -7,18 +7,31 @@
  * License: GNU General Public License v2 or later
  */
 
-  function wporg_custom_post_type() {
-      register_post_type('wporg_product',
-        array(
-          'labels' => array(
-            'name'          => __('Products', 'textdomain'),
-            'singular_name' => __('Product', 'textdomain'),
-          ),
-            'public'      => true,
-            'has_archive' => true,
-        ),
-      );
-    };
-
-    add_action('init', 'wporg_custom_post_type');
+function tk_portfolio_item() {
+  $labels = array(
+    'name'               => _x( 'Products', 'post type general name' ),
+    'singular_name'      => _x( 'Product', 'post type singular name' ),
+    'add_new'            => _x( 'Add New', 'book' ),
+    'add_new_item'       => __( 'Add New Product' ),
+    'edit_item'          => __( 'Edit Product' ),
+    'new_item'           => __( 'New Product' ),
+    'all_items'          => __( 'All Products' ),
+    'view_item'          => __( 'View Product' ),
+    'search_items'       => __( 'Search Products' ),
+    'not_found'          => __( 'No products found' ),
+    'not_found_in_trash' => __( 'No products found in the Trash' ), 
+    'parent_item_colon'  => ’,
+    'menu_name'          => 'Products'
+  );
+  $args = array(
+    'labels'        => $labels,
+    'description'   => 'Holds our products and product specific data',
+    'public'        => true,
+    'menu_position' => 5,
+    'supports'      => array( 'title', 'editor', 'thumbnail', 'excerpt', 'comments' ),
+    'has_archive'   => true,
+  );
+  register_post_type( 'product', $args ); 
+}
+add_action( 'init', 'my_custom_post_product' );
 ?>
