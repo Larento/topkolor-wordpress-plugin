65f4108d76ea9be2f21accba5580845056b34266
Larik <turolretar@gmail.com>
2020-08-13 15:25:08 +0300

    Updated Plugin

--- topkolor-wordpress-plugin.php
+++ topkolor-wordpress-plugin.php
@@ -9,29 +9,17 @@
 
 function tk_portfolio_item() {
   $labels = array(
-    'name'               => _x( 'Products', 'post type general name' ),
-    'singular_name'      => _x( 'Product', 'post type singular name' ),
-    'add_new'            => _x( 'Add New', 'book' ),
-    'add_new_item'       => __( 'Add New Product' ),
-    'edit_item'          => __( 'Edit Product' ),
-    'new_item'           => __( 'New Product' ),
-    'all_items'          => __( 'All Products' ),
-    'view_item'          => __( 'View Product' ),
-    'search_items'       => __( 'Search Products' ),
-    'not_found'          => __( 'No products found' ),
-    'not_found_in_trash' => __( 'No products found in the Trash' ), 
-    'parent_item_colon'  => ’,
-    'menu_name'          => 'Products'
+    'name'               => _x( 'Portfolio Item', 'post type general name' ),
+    'menu_name'          => 'Portfolio Items',
   );
   $args = array(
     'labels'        => $labels,
-    'description'   => 'Holds our products and product specific data',
     'public'        => true,
     'menu_position' => 5,
-    'supports'      => array( 'title', 'editor', 'thumbnail', 'excerpt', 'comments' ),
+    'supports'      => array( 'title', 'editor', 'thumbnail', 'excerpt',),
     'has_archive'   => true,
   );
-  register_post_type( 'product', $args ); 
+  register_post_type( 'portfolio_item', $args ); 
 }
 add_action( 'init', 'tk_portfolio_item' );
 ?>
