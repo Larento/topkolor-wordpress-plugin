2a12f5abc0a5a574c9293aa6ae252c56f8220493
Larik <turolretar@gmail.com>
2020-08-13 14:07:40 +0300

    Create topkolor-wordpress-plugin.php

---
+++ topkolor-wordpress-plugin.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Plugin Name: TOPKOLOR Plugin
+ * Author: Larik
+ * Version: 0.0.1
+ * GitHub Plugin URI: https://github.com/Larento/topkolor-wordpress-plugin
+ * License: GNU General Public License v2 or later
+ */
+
+  function wporg_custom_post_type() {
+      register_post_type('wporg_product',
+        array(
+          'labels' => array(
+            'name'          => __('Products', 'textdomain'),
+            'singular_name' => __('Product', 'textdomain'),
+          ),
+            'public'      => true,
+            'has_archive' => true,
+        ),
+      );
+    };
+
+    add_action('init', 'wporg_custom_post_type');
+?>
