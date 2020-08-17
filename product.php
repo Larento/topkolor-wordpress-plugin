6c921faced5394d8483805188e01648fa160bb77
Larik <turolretar@gmail.com>
2020-08-17 20:13:43 +0300

    Updated Plugin

--- product.php
+++ product.php
@@ -129,7 +129,11 @@
   function tk_get_current_product_kind() {
     global $post;
     $product = tk_get_current_product();
-    return ( tk_is_product() === true ) ? get_the_terms( $post, tk_taxonomy_name('', tk_get_product_slug($product)) ) : 'not_product_kind';
+    if ( tk_is_product() === true ) {
+      return ( get_the_terms( $post, tk_taxonomy_name('', tk_get_product_slug($product)) ) !== false ) ? get_the_terms( $post, tk_taxonomy_name('', tk_get_product_slug($product)) ) : 'not_product_kind';
+    } else {
+      return 'not_product';
+    };
   };
 
   function tk_is_product_kind() {
