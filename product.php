318c7ed77fc4a26f1de1fc3012464c2378b75da3
Larik <turolretar@gmail.com>
2020-08-18 11:34:51 +0300

    Updated Plugin

--- product.php
+++ product.php
@@ -124,6 +124,8 @@
     return get_terms([
       'taxonomy'    => tk_taxonomy_name('', tk_get_product_slug($product)),
       'hide_empty'  => false,
+      'order'       => 'date',
+      'orderby'     => 'ASC',
     ]);
   };
 
