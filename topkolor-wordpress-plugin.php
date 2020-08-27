e521a576a80543b094c13dc999af2fb22789ab1f
Larik <turolretar@gmail.com>
2020-08-27 22:44:43 +0300

    Updated Plugin

--- topkolor-wordpress-plugin.php
+++ topkolor-wordpress-plugin.php
@@ -12,7 +12,7 @@ include_once( plugin_dir_path(__FILE__) . '/product/product_classes.php' );
 include_once( plugin_dir_path(__FILE__) . '/product/product_functions.php' );
 include_once( plugin_dir_path(__FILE__) . '/product/product_rest_controller.php' );
 
-$tk_products = tk\register_products( '/product/product_types.json' );
+$tk_products = tk\register_products( plugin_dir_path(__FILE__) . '/product/product_types.json' );
 
 add_action( 'rest_api_init', 'prefix_register_my_rest_routes' );
 function prefix_register_my_rest_routes() {
