12c13e505707473d38779e4c4e46e83b6a58ce81
Larik <turolretar@gmail.com>
2020-08-28 17:06:32 +0300

    Updated Plugin

--- topkolor-wordpress-plugin.php
+++ topkolor-wordpress-plugin.php
@@ -2,30 +2,23 @@
 /**
  * Plugin Name: TOPKOLOR Plugin
  * Author: Larik
- * Version: 0.1.8
+ * Version: 0.1.9
  * GitHub Plugin URI: https://github.com/Larento/topkolor-wordpress-plugin
  * License: GNU General Public License v2 or later
  */
 use \tk\functions as tk;
 
+include_once( plugin_dir_path(__FILE__) . '/functions.php' );
+include_once( plugin_dir_path(__FILE__) . '/product/product_classes.php' );
+include_once( plugin_dir_path(__FILE__) . '/product/product_functions.php' );
+include_once( plugin_dir_path(__FILE__) . '/product/product_rest_controller.php' );
 
+$tk_products = tk\register_products( plugin_dir_path(__FILE__) . '/product/product_types.json' );
 
-// $tk_products = [];
+add_action( 'init', tk\get_handle('products_init') );
 
-// add_action ( 'plugins_loaded', 'load_plugin');
-// function load_plugin() {
-  include_once( plugin_dir_path(__FILE__) . '/functions.php' );
-  include_once( plugin_dir_path(__FILE__) . '/product/product_classes.php' );
-  include_once( plugin_dir_path(__FILE__) . '/product/product_functions.php' );
-  include_once( plugin_dir_path(__FILE__) . '/product/product_rest_controller.php' );
-  // global $tk_products;
-  $tk_products = tk\register_products( plugin_dir_path(__FILE__) . '/product/product_types.json' );
-
-  //add_action( 'init', tk\get_handle('products_init') );
-
-  add_action( 'rest_api_init', 'prefix_register_my_rest_routes' );
-  function prefix_register_my_rest_routes() {
-    $controller = new \tk\classes\product_rest_controller();
-    $controller->register_routes();
-  }
-//}
+add_action( 'rest_api_init', 'prefix_register_my_rest_routes' );
+function prefix_register_my_rest_routes() {
+  $controller = new \tk\classes\product_rest_controller();
+  $controller->register_routes();
+}
